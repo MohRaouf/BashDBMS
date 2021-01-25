eb99c318c52c80af2f07cd6201c6a39a8a6a31ba
MohRaouf <moh.proff@gmail.com>
2021-01-25 23:42:41 +0200

    Refine integration

--- drop_db.sh
+++ drop_db.sh
@@ -2,16 +2,11 @@
 read -p  "Enter Database Name : " name
 if [ -d DBMS/$name ]
 then
-        echo "Are you sure You Want To Delete $name?"
-        read choice
+        read -p "Are you sure You Want To Delete $name ? (Y/N) : " choice
         case $choice in
-                [yY]*)
-                        rm -r DBMS/$name
-                        echo "$name Deleted Successfully";;
-                [nN]*)
-                        echo " Operation Canceled";;
-                *)
-                        echo "Invalid option";;
+                [yY]*) rm -r DBMS/$name; echo "$name Deleted Successfully";;
+                [nN]*) echo " Operation Canceled";;
+                *) echo "Invalid option";;
         esac
 else
         echo "$name Database Not Exist"
--- drop_tb.sh
+++ drop_tb.sh
@@ -2,12 +2,9 @@
 read -p "Enter Table Name To Be Deleted: " name
 if [ -f $name ]
 then 
-	echo "Are You Sure You Want To Delete $name Table?"
-	read choice
+	read -p "Are You Sure You Want To Delete $name Table ? (Y/N) : " choice
 	case $choice in
-		[yY]*) rm $name 
-			echo "Table Deleted Successfully" ;;
-
+		[yY]*) rm $name; echo "Table Deleted Successfully" ;;
 		[nN]*) echo "Canceled";;
 		*) echo "Invalid Option";;
 	esac
