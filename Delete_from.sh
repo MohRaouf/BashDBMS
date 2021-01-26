7a4caa7dc80a4e8762ed8461e8fe5db9044b84f7
Amaal Asaad <amal.asaad26@gmail.com>
2021-01-27 00:11:16 +0200

    v1 Delete from table

---
+++ Delete_from.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+read -p "Enter Table Name: " name
+if [ -f $name ]
+then
+	awk 'BEGIN{FS="|"}{if(NR==1){print $0}}' $name
+	read -p "Enter column to delete record from:  " coldel
+	read -p "Enter value: " vldel
+	awk 'BEGIN{FS="|"}
+	{
+		if(NR==1){
+			for(i=1;i<=NF;i++){
+				if($i=="'$coldel'"){var=i}
+			}
+		}
+		else{
+			if($var=='$vldel'){
+				loc=NR
+			}
+		}
+		{if(NR!=loc)print
+		}
+	}' $name > tmp && mv tmp $name
+
+	echo "Row Deleted Successfully"
+else
+	echo "$name Not Exist"
+fi
