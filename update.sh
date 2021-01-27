60762c72f0349fd08854d3bf1d5435d9f35504db
MohRaouf <moh.proff@gmail.com>
2021-01-28 01:59:06 +0200

    Finish update - all checks

--- update.sh
+++ update.sh
@@ -12,19 +12,19 @@ table_name=$(echo "$sql_line" | awk -F';' '{gsub(/^[ \t]+|[ \t]+$/, "",$1);print
 
 #get the column in the WHERE condition and check its existance
 column_name=$(echo "$sql_line" | awk -F';' '{print $3}' | awk -F'=' '{gsub(/^[ \t]+|[ \t]+$/, "",$1);print $1}')
-found=$(awk 'BEGIN{FS="|"; found=0} {if(NR==1){for(i=1;i<=NF;i++){if($i=="'$column_name'")found=1}}} END{print found}' "$table_name")
+found=$(awk -F'|' 'BEGIN{found=0} {if(NR==1){for(i=1;i<=NF;i++){if($i=="'$column_name'")found=1}}} END{print found}' "$table_name")
 if ((found == 0)); then exit 2; fi
 
 #get the value in the WHERE condition and check its existance
 selected_record=$(echo "$sql_line" | awk -F';' '{print $3}' | awk -F'=' '{gsub(/^[ \t]+|[ \t]+$/, "",$2);print $2}')
-line_no=$(awk 'BEGIN{FS="|"; found=0} {if(NR!=1){for(i=1;i<=NF;i++){if($i=="'$selected_record'")found=NR}}} END{print found}' "$table_name")
-if ((line_no == 0)); then exit 3; fi
+record_field=$(awk -F'|' 'BEGIN{found=0} {if(NR!=1){for(i=1;i<=NF;i++){if($i=="'$selected_record'")found=i}}} END{print found}' "$table_name")
+if ((record_field == 0)); then exit 3; fi
 
 #get the column in the SET
 update_col_name=$(echo "$sql_line" | awk -F';' '{print $2}' | awk -F'=' '{gsub(/^[ \t]+|[ \t]+$/, "",$1);print $1}')
-field_no=$(awk 'BEGIN{FS="|"; found=0} {if (NR==1){for(i=1;i<=NF;i++){if($i=="'$update_col_name'")found=i}}} END{print found}' "$table_name")
-col_data_type=$(awk 'BEGIN{FS="|"} {if ($1=="'$update_col_name'"){print $2}}' ".$table_name")
-is_pk=$(awk 'BEGIN{FS="|"} {if ($1=="'$update_col_name'"){print $3}}' ".$table_name")
+field_no=$(awk -F'|' 'BEGIN{found=0} {if (NR==1){for(i=1;i<=NF;i++){if($i=="'$update_col_name'")found=i}}} END{print found}' "$table_name")
+col_data_type=$(awk -F'|' '{if ($1=="'$update_col_name'"){print $2}}' ".$table_name")
+is_pk=$(awk -F'|' '{if ($1=="'$update_col_name'"){print $3}}' ".$table_name")
 if ((field_no == 0)); then exit 2; fi
 
 #validate the new value with the column type
@@ -37,11 +37,9 @@ esac
 
 # check if the update value in pk column and the new value exists
 if ((is_pk == "pk")); then
-        value_exist=$(awk 'BEGIN{FS="|"; found=0} {if(NR!=1 && NR !="'$line_no'"){for(i=1;i<=NF;i++){if($i=="'$new_value'")found=1}}} END{print found}' "$table_name")
+        value_exist=$(awk -v new_value="$new_value" -F'|' 'BEGIN{found=0} {if(NR!=1){for(i=1;i<=NF;i++){if($"'$selected_record'"==new_value)found=1}}} END{print found}' "$table_name")
         if ((value_exist == 1)); then exit 5; fi
 fi
 
-# line_no=$(awk 'BEGIN{FS="|"; found=0} {if(NR!=1){for(i=1;i<=NF;i++){if($i=="'$selected_record'")found=NR}}} END{print found}' "$table_name")
-
 #update the selected record with the new value
-awk -v new_value="$new_value" -i inplace -F'|' '{OFS=FS}{if (NR=="'$line_no'"){ $"'$field_no'"=new_value } print}' "$table_name" 2>../../error.log
+awk -v new_value="$new_value" -i inplace -F'|' '{OFS=FS}{if($"'$record_field'"=="'$selected_record'"){$"'$field_no'"=new_value } print}' "$table_name" 2>../../error.log
